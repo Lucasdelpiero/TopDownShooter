fc36e9f37c480195036e152a9a8c6e06b57a9c2c
Lucas <lucasdelpiero98@gmail.com>
2021-01-25 18:42:30 -0300

    Added rolling barrels and sound effects for hitting bullets

--- World/Objects/Barrel/ExplosiveBarrel.gd
+++ World/Objects/Barrel/ExplosiveBarrel.gd
@@ -1,15 +1,63 @@
-extends StaticBody2D
+extends KinematicBody2D
 
 export var health = 2
 var exploted = false
 onready var Explosion = preload("res://World/Objects/Barrel/Explosion.tscn")
+onready var BarrelDown = preload("res://World/Objects/Barrel/barrel_exp_down 197x128.png")
+onready var ShapeRectangle = preload("res://World/Objects/Barrel/shapeRectangle.tres")
+onready var FireParticle = preload("res://World/Objects/Barrel/FireParticle.tscn")
+onready var audioStreamPlayer = $AudioStreamPlayer
 onready var timer = $Timer
+onready var sprite = $Sprite
+onready var areaCollision = $HurtBox/CollisionShape2D
+onready var areaCollisionRolling = $HurtBox/CollisionRolling
+onready var collisionShape = $CollisionShape2D
+onready var collisionShapeRolling = $CollisionRolling
+export var speed = 500
+var direction = Vector2.ZERO
+var collision = false
+var moving = false
+var onFire = false
+
+var soundsHitted = [
+	"barrel_shot_1",
+	"barrel_shot_2",
+]
 
 func _ready():
 	randomize()
 
+func _physics_process(delta):
+#	velocity = velocity.move_toward(direction * speed, delta)
+	if moving:
+		var vector = Vector2( cos(direction), sin(direction) )
+#		velocity = move_and_slide(vector * speed)
+		collision = move_and_collide(vector * speed * delta)
+		if collision:
+			moving = false
+			if onFire:
+				createExplosion()
+#		if collisionShape.is_colliding():
+#			pass
+
 func _on_HurtBox_area_entered(area):
-	health -= area.damage
+	if area.is_in_group("Melee"):
+		direction = area.get_parent().get_parent().rotation
+		rotation = direction
+		sprite.texture = BarrelDown
+		moving = true
+		toggleCollisionShape()
+	else:
+		health -= area.damage
+		if not onFire:
+			var fireParticle = FireParticle.instance()
+			add_child(fireParticle)
+			fireParticle.global_position = global_position
+			onFire = true
+			timer.start(rand_range(2, 3))
+			audioStreamPlayer.stream = load( "res://World/Objects/Barrel/%s.wav" %str(soundsHitted[randi() % soundsHitted.size()]) ) 
+			audioStreamPlayer.play()
+
 	if health < 1 and not exploted:
 		#Called deferred because of a bug
 		exploted = true
@@ -21,13 +69,10 @@ func createExplosion():
 	var explosion = Explosion.instance()
 	get_parent().add_child(explosion)
 	explosion.global_position = global_position
-	health = 100000
 	queue_free()
 
-
-
-
-
+func toggleCollisionShape():
+	areaCollision.set_shape(ShapeRectangle)
 
 func _on_Timer_timeout():
 	createExplosion()
--- World/Objects/Bullet.gd
+++ World/Objects/Bullet.gd
@@ -6,13 +6,19 @@ onready var direction = 1 # rotation_degrees
 var velocity = Vector2.ZERO
 var motion = Vector2.ZERO
 onready var rayCast = $Sprite/RayCast2D
+onready var audio = $AudioStreamPlayer
+onready var timer = $Timer
+var collided = false
 
 # Called when the node enters the scene tree for the first time.
 #func _ready():
 #	direction = get_angle_to(get_global_mouse_position())
 #	sprite.rotation_degrees = rad2deg(direction)
 
-
+var bulletHitWall = [
+	"bullet_hit_wall_1",
+	"bullet_hit_wall_2",
+]
 
 func _process(delta):
 	move(delta)
@@ -23,8 +29,14 @@ func move(delta):
 	motion.x = speed * cos(direction)
 	motion.y = speed * sin(direction)
 	move_and_collide(motion)
-	if rayCast.is_colliding():
-		queue_free()
+	if rayCast.is_colliding() and not collided:
+		audio.stream = load ("res://World/Objects/%s.wav" % str(bulletHitWall[ randi() % bulletHitWall.size() ] ) )
+		audio.play()
+		speed = 0
+		timer.start(0.5)
+		sprite.visible = false
+		collided = true
+#		queue_free()
 
 func _on_Timer_timeout():
 	queue_free()
