edaaaebb34896f13be1171bd5cfaaa943f2da8c0
Lucas <lucasdelpiero98@gmail.com>
2021-01-28 04:03:01 -0300

    Added combo time bar

--- HUD/Scoring.gd
+++ HUD/Scoring.gd
@@ -4,10 +4,14 @@ var totalScore = 0         # Total Score
 var individualScore = 0    # Last action score
 var comboScore = 0         # Total individual score in the combo
 var multiplicator = 0      # Multiplicator to all kill in the combo
+var comboBarValue = 50
+export var comboTime = 2
 
 onready var comboLabel = $ComboLabels/Score
 onready var multiplicatorLabel = $ComboLabels/Multiplicator
 onready var totalLabel = $TotalScoreLabel
+onready var comboBar = $ComboLabels/ComboBar
+onready var tweenComboBar = $ComboLabels/ComboBar/Tween
 
 var dictionary = {
 	"zombi" : 200,
@@ -25,12 +29,13 @@ var dictionary = {
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	startCombo()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-#	totalLabel.text = str(dictionary["zombi"])
+#	if timerCombo.is_stopped():
+#		timerCombo.start()
 	pass
 
 func updateScore(name):
@@ -39,7 +44,42 @@ func updateScore(name):
 	comboScore += base
 	multiplicator += 1
 	totalScore += 5000
+	
+	updateLabels()
+	
+	resetCombo()
+
+	
+
+func startCombo():
+	tweenComboBar.interpolate_property(comboBar, "value", 100, 0, comboTime, Tween.TRANS_LINEAR)
+	tweenComboBar.start()
+	changeVisibility(true)
+
+func resetCombo():
+	tweenComboBar.interpolate_property(comboBar, "value", 0, 100, 0, Tween.TRANS_LINEAR)
+	tweenComboBar.start()
+	startCombo()
+
+func endCombo():
+	comboScore = 0
+	multiplicator = 0
+	updateLabels()
+	changeVisibility(false)
+	#add total score obtained in the combo
+
+func _on_Tween_tween_all_completed():
+	if comboBar.value == 0:
+		endCombo()
+
+func updateLabels():
 	comboLabel.text = str(comboScore)
 	multiplicatorLabel.text = " x " + str(multiplicator)
 	totalLabel.text = str(totalScore)
-	
+
+func changeVisibility(value):
+	comboBar.visible = value
+	comboLabel.visible = value
+	multiplicatorLabel.visible = value
+
+
