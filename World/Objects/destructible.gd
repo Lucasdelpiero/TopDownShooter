bb773d01823b9b052c6f89949c71c5613b7c4fa8
Lucas <lucasdelpiero98@gmail.com>
2021-02-25 00:13:40 -0300

    Added breakable door

---
+++ World/Objects/destructible.gd
@@ -0,0 +1,12 @@
+extends StaticBody2D
+
+onready var sprite = $Sprite
+var DoorParticles = preload("res://World/Objects/Decoration/DoorParticles.tscn")
+
+func _on_Area2D_area_entered(area):
+	var doorParticles = DoorParticles.instance()
+	get_tree().get_root().add_child(doorParticles)
+	doorParticles.global_position = global_position
+	doorParticles.rotation = rotation
+	queue_free()
+
