50a307bf1c6a25935a3ab508db313fac7c6a37a7
Lucas <lucasdelpiero98@gmail.com>
2021-03-05 22:24:35 -0300

    Added Optional Objectives

--- HUD/Objectives.gd
+++ HUD/Objectives.gd
@@ -1,23 +1,32 @@
 extends Control
 
-# Win conditions
+# Required objectives
 export var killAll = false
 export var survive = false
-
 export(float, 2.0, 60.0, 5.0) var timeSurvive = 2.0
-#export(Array, bool) var enums 
-#export(Array, int, "Red", "Green", "Blue") var enums = [2, 1, 0]
-#export(int, FLAGS, "kill all", "survive", "k/w explosives", "k/w melee") var objectives = 0
 
+# Optional objectives
+export var withMelee = false
+export(int, 1, 100, 1) var meleeAmount = 1
+export var withExplosion = false
+export(int, 1, 100, 1) var explosionAmount = 1
 
+# Required completion
 var killAllCompleted = false
 var surviveCompleted = false
 
+# Optional completion
+var withMeleeCompleted = false
+var withExplosionCompleted = false
 
+# Lists
 var objectives = [ killAll, survive ]
 var currentObjectives = [killAllCompleted, surviveCompleted]
 
+var optionalObjectives = [withMelee ,withExplosion]
+var currentOptional = [withMeleeCompleted, withExplosionCompleted]
 
+# Count
 var zombiesLeft = 0
 var killedByExplosion = 0
 var killedByMelee = 0
@@ -27,7 +36,9 @@ onready var label = $CanvasLayer/Base/Label
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	zombiesLeft = get_tree().get_nodes_in_group("zombi").size()
-	objectives = [ killAll, survive ]
+	# Updating the arrays with the changes made in editor
+	objectives = [ killAll, survive ] 
+	optionalObjectives = [withMelee ,withExplosion]
 	addObjectives()
 	if survive:
 		$TimerSurvive.start(timeSurvive)
@@ -35,26 +46,56 @@ func _ready():
 func updateObjective(name, byMelee, byExplosion):
 #	zombiesLeft =  get_tree().get_nodes_in_group("zombi").size()
 	zombiesLeft -= 1
+	if byMelee:
+		killedByMelee += 1
+	if byExplosion:
+		killedByExplosion += 1
+	
+#	print("Melee " +str(byMelee))
+#	print("Explosion " +str(byExplosion))
 	checkCompletion()
 
-func checkCompletion():
+func checkCompletion(): 
+	
 	if zombiesLeft < 1:
 		killAllCompleted = true
+	if withMelee:
+		if killedByMelee == meleeAmount:
+			withMeleeCompleted = true
+	if withExplosion:
+		if killedByExplosion == explosionAmount:
+			withExplosionCompleted = true
+	
 	updateCompletion()
+	checkOptional()
+
 	for i in currentObjectives.size():
 		if currentObjectives[i] == false:
 			return #If any objective not completed it returns
-	completed()
+	completed() # Win
 
-func updateCompletion():
+func checkOptional():
+	print(currentOptional)
+	for i in currentOptional.size():
+		if currentOptional[i] == false:
+			return
+			
+	print("OPTIONALS COMPLETED")
+
+func updateCompletion(): # Update content of arrays
 	currentObjectives = [killAllCompleted, surviveCompleted]
+	currentOptional = [withMeleeCompleted, withExplosionCompleted]
 	addObjectives()
 
-func addObjectives():
+func addObjectives(): # Keep only active objectives
 	for i in objectives.size():
 		if objectives[i] == false:
 			currentObjectives.remove(i)
-	print("objectives" + str(currentObjectives))
+	
+	for i in optionalObjectives.size():
+		if optionalObjectives[i] == false:
+			currentOptional.remove(i)
+#	print("objectives" + str(currentOptional))
 
 func completed():
 	label.visible = true
