696e89304dc7104118a1e1073eb38244829e1cc1
Lucas Del Piero <lucasdelpiero98@gmail.com>
2022-08-26 21:42:28 -0300

    Diamonds in level selector now animated

--- HUD/Main Menu/Level Selector/LevelButton.gd
+++ HUD/Main Menu/Level Selector/LevelButton.gd
@@ -2,11 +2,13 @@ extends MenuButtonClass
 
 signal diamondChosen(name)
 
-
 func _on_LevelButton_pressed():
 	emit_signal("diamondChosen", name)
 	audioPressed.play()
 
-
 func _on_LevelButton_mouse_entered():
 	audioHover.play()
+	anim_set_large()
+
+func _on_LevelButton_mouse_exited():
+	anim_set_normal()
