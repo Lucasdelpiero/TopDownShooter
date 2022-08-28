98b3a248285ae43b94c4db9a74e4faebba84c07b
Lucas Del Piero <lucasdelpiero98@gmail.com>
2022-08-28 00:57:57 -0300

    Fixed lag at level selector

--- HUD/Main Menu/TitleScreen.gd
+++ HUD/Main Menu/TitleScreen.gd
@@ -2,12 +2,12 @@ extends Control
 
 onready var options = $OptionsLayer/Options 
 onready var transition = $Transition/AnimationPlayer
+var levelSelector = load("res://HUD/Main Menu/Level Selector/LevelSelector.tscn")
 
 func _on_StartButton_pressed():
 	transition.play("Fade In")
-#	yield(get_tree().create_timer(0.5), "timeout")
 	yield(transition, "animation_finished")
-	get_tree().change_scene("res://HUD/Main Menu/Level Selector/LevelSelector.tscn")
+	get_tree().change_scene_to(levelSelector)
 
 func _on_ExitButton_pressed():
 	get_tree().quit()
